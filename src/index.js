30ecccc5b1d145c3166418c193eddd9c181e11ae
IharTsykaka <35345292+IharTsykaka@users.noreply.github.com>
2018-02-26 16:21:08 +0300

    task 03

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
 class Sorter {
   constructor() {
-    // your implementation
+      this.array = [];
+      this.compareFunction = (a, b) => a - b;
   }
-
-  add(element) {
-    // your implementation
+  add(elem) {
+      this.array.push(elem);
   }
-
   at(index) {
-    // your implementation
+      return this.array[index];
   }
-
   get length() {
-    // your implementation
+      return this.array.length;
   }
-
   toArray() {
-    // your implementation
+      return this.array;
   }
-
   sort(indices) {
-    // your implementation
+      var mas = [];
+      indices.sort();
+      for (var i = 0; i < indices.length; i++) {
+          mas.push(this.array[indices[i]]);
+      }
+      mas.sort(this.compareFunction);
+      for (var i = 0; i < indices.length; i++) {
+          this.array[indices[i]] = mas[i];
+      }
   }
-
   setComparator(compareFunction) {
-    // your implementation
+      this.compareFunction = compareFunction;
   }
-}
+};
 
 module.exports = Sorter;
